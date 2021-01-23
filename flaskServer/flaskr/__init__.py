fe43afd9763f7bbc407c943e823dced6eec04b48
engelsa <engelsa@purdue.edu>
2021-01-23 14:53:30 -0500

    Added flask server

---
+++ flaskServer/flaskr/__init__.py
@@ -0,0 +1,23 @@
+import os
+import sqlite3
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return "Hello world!"
+
+    return app
