9a82ba2240712c9343a0f97b06a79e37a4ea49f9
engelsa <engelsa@purdue.edu>
2021-01-24 00:41:07 -0500

    climate adjustments

--- flaskServer/flaskr/__init__.py
+++ flaskServer/flaskr/__init__.py
@@ -21,64 +21,93 @@ def config_app():
 @app.route('/hello')
 def hello():
     parameters = {
-        "politics": {
-            "quantity": 50,
-            "importance": 30
-        },
         "cost": {
-            "importance": 10
-        },
-        "transport": {
-            "importance": 70
-        },
-        "crime": {
-            "importance": 100
-        },
-        "education": {
-            "importance": 100
-        },
-        "climate": {
-            "high": 100,
-            "low": 20,
-            "importance": 45
-        },
-        "density": {
-            "quantity": 80,
-            "importance": 100
-        },
-        "elevation": {
-            "quantity": 1000,
-            "importance": 10
+            "importance": 50
         },
         "age": {
             "quantity": 25,
+            "importance": 30
+        },
+        "crime": {
+            "importance": 80
+        },
+        "income": {
+            "importance": 90
+        },
+        "travel_time": {
+            "importance": 30
+        },
+        "employment": {
+            "importance": 95
+        },
+        "disability": {
+            "importance": 10
+        },
+        "education": {
             "importance": 90
         },
         "airport": {
-            "importance": 15
+            "importance": 25
         },
-        "disability": {
-            "importance": 5
+        "transportation": {
+            "importance": 100
+        },
+        "in_tax": {
+            "importance": 25
+        },
+        "climate": {
+            "high": 120,
+            "low": 70,
+            "importance": 100
         }
     }
 
     connection = sqlite3.connect('../data.db')
     cursor = connection.cursor()
 
-    query = """SELECT cities.city, cities.state, cost_living.ind *
-                   {} AS _cost, (age.age -
-                    {}) *
-                   {} AS _age, crime.murders * 
-                   {} AS _crime 
-                   FROM cities INNER JOIN cost_living ON (cost_living.city=cities.city AND 
-                   cost_living.state_abbrev=cities.state_abbrev) INNER JOIN age ON 
-                   (age.city=cities.city AND age.state_abbrev=cities.state_abbrev) INNER JOIN 
-                   crime ON (crime.state=cities.state)"""\
+    query = """SELECT DISTINCT cities.city, cities.state, (1 - (cost_living.ind / 183)) * {} AS _cost,
+                    1 / ((ABS(age.age - {}) / age.age) + 1) * {} AS _age,
+                   (1 - (crime.murders / 12.4)) * {} AS _crime,
+                   employment.employed / employment.civ_force * {} AS _employment,
+                   employment.house_income / 130865 * {} AS _income,
+                   (1 - (employment.travel_time / 35.9)) * {} AS _travel_time,
+                   disability.score / 57 * {} AS _disability,
+                   ed.spending / 23091 * {} AS _ed,
+                   (1 - (airport.dist / 1400)) * {} AS _airport,
+                   transportation.miles / 10000000000 * {} AS _transportation,
+                   (1 - ((in_tax.high + in_tax.low) / 24.6)) * {} AS _in_tax,
+                   (1 / (ABS(climate.high - {}) / climate.high + 1) + 
+                   1 / (ABS(climate.low - {}) / climate.low + 1)) * {} AS _climate 
+                   FROM cities INNER JOIN cost_living ON (LOWER(cost_living.city)=LOWER(cities.city) AND 
+                   LOWER(cost_living.state_abbrev)=LOWER(cities.state_abbrev)) LEFT JOIN age ON 
+                   (LOWER(age.city)=LOWER(cities.city) AND LOWER(age.state_abbrev)=LOWER(cities.state_abbrev)) INNER JOIN 
+                   crime ON (LOWER(crime.state)=LOWER(cities.state)) INNER JOIN employment ON 
+                   (LOWER(employment.city)=LOWER(cities.city) AND 
+                   LOWER(employment.state_abbrev)=LOWER(cities.state_abbrev)) LEFT JOIN disability 
+                   ON (LOWER(disability.city)=LOWER(cities.city) AND LOWER(disability.state_abbrev)=LOWER(cities.state_abbrev)) 
+                   INNER JOIN ed ON (LOWER(ed.state)=LOWER(cities.state)) LEFT JOIN airport 
+                   ON (LOWER(airport.city)=LOWER(cities.city) AND LOWER(airport.state_abbrev)=LOWER(cities.state_abbrev)) 
+                   LEFT JOIN transportation ON (LOWER(transportation.city)=LOWER(cities.city) AND 
+                   LOWER(transportation.state_abbrev)=LOWER(cities.state_abbrev)) INNER JOIN in_tax ON 
+                   LOWER(in_tax.state)=LOWER(cities.state) INNER JOIN climate ON (LOWER(climate.city)=LOWER(cities.city) AND LOWER(climate.state)=LOWER(cities.state)) ORDER BY 
+                   _age * _age + _crime * _crime + _employment * _employment + _income * _income 
+                   + _travel_time * travel_time + _disability * _disability + _ed * _ed + 
+                   _airport * _airport + _transportation * _transportation + _climate * _climate DESC"""\
                     .format(str(parameters["cost"]["importance"]),
                             str(parameters["age"]["quantity"]),
                             str(parameters["age"]["importance"]),
-                            str(parameters["crime"]["importance"])
-                            )
+                            str(parameters["crime"]["importance"]),
+                            str(parameters["employment"]["importance"]),
+                            str(parameters["income"]["importance"]),
+                            str(parameters["travel_time"]["importance"]),
+                            str(parameters["disability"]["importance"]),
+                            str(parameters["education"]["importance"]),
+                            str(parameters["airport"]["importance"]),
+                            str(parameters["transportation"]["importance"]),
+                            str(parameters["in_tax"]["importance"]),
+                            str(parameters["climate"]["high"]),
+                            str(parameters["climate"]["low"]),
+                            str(parameters["climate"]["importance"]))
 
     print(query)
 
